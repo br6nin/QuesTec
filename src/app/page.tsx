e74f383c728a880d0275b08470e6aba74e6a791a
br6nin <bruno456zica@gmail.com>
2025-11-19 15:19:08 -0300

    alteração no frontend homepage 19/11/2025

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -99,7 +99,7 @@ export default function Home() {
               <div className="w-10 h-10 bg-red-600 rounded-lg flex items-center justify-center">
                 <PenTool className="w-6 h-6 text-white" />
               </div>
-              <span className="text-2xl font-bold text-gray-900">QuesTec</span>
+              <a href= "https://questec.vercel.app" className="text-2xl font-bold text-gray-900">QuesTec</a>
             </div>
             <nav className="hidden md:flex items-center space-x-6">
               <a href="#how" className="text-gray-600 hover:text-gray-900 transition">Como Funciona</a>
