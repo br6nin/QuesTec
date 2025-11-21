75af0c9b536957b088b3140c565e4604f6540f0c
br6nin <bruno456zica@gmail.com>
2025-11-21 17:16:15 -0300

    Automação dos pagamentos Stripe 21-11-2025

--- src/app/plans/page.tsx
+++ src/app/plans/page.tsx
@@ -105,7 +105,7 @@ const plans: Plan[] = [
     color: 'ring-gray-300',
     paymentRequired: true,
     creditsToAdd: 5,
-    priceId: 'price_1PBasicoXXXXXX', // <-- SUBSTITUA ESTE ID
+    priceId: 'price_1SVyEp7y7k1z73nwQrreXq4y', // <-- SUBSTITUA ESTE ID
   },
   {
     name: 'Plus',
@@ -122,7 +122,7 @@ const plans: Plan[] = [
     color: 'ring-indigo-600',
     paymentRequired: true,
     creditsToAdd: 15,
-    priceId: 'price_1PPlusXXXXXX', // <-- SUBSTITUA ESTE ID
+    priceId: 'price_1SVyFZ7y7k1z73nwWRLqyz3Q', // <-- SUBSTITUA ESTE ID
   },
   {
     name: 'Premium',
@@ -140,7 +140,7 @@ const plans: Plan[] = [
     color: 'ring-gray-300',
     paymentRequired: true,
     creditsToAdd: 30,
-    priceId: 'price_1PPremiumXXXXXX', // <-- SUBSTITUA ESTE ID
+    priceId: 'price_1SVyFz7y7k1z73nwT56tbTH6', // <-- SUBSTITUA ESTE ID
   }
 ]
 
